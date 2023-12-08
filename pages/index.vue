57bf6ee4e9ffa488608c45b36fdb6def008eabe9
Arthur Segato <arthusegato@protonmail.com>
2023-12-08 03:02:02 +0000

    Add error pages

--- pages/index.vue
+++ pages/index.vue
@@ -33,8 +33,6 @@ useSeoMeta({
 </script>
 
 <template>
-  <span
-    class="w-full h-2 absolute top-0 left-0 bg-red-600 sm:bg-orange-600 md:bg-yellow-500 lg:bg-green-500 xl:bg-blue-600 2xl:bg-purple-600"></span>
   <main
     class="font-['Open_Sans'] flex min-h-screen w-full items-center justify-center bg-[#FBFBFB] dark:bg-[#29292a] dark:text-[#FBFBFB] text-[#29292a]">
     <div class="fade-in-fwd flex flex-col items-center justify-center gap-5 xl:gap-10" v-if="!isSubmitted">
