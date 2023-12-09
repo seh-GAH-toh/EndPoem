641b69b4e471f50d423069ab499c957f33e51d08
Arthur Segato <arthusegato@protonmail.com>
2023-12-09 22:27:13 +0000

    Remove unused packges

--- .nuxt/app.config.mjs
+++ .nuxt/app.config.mjs
@@ -4,7 +4,7 @@ import { defuFn } from 'defu'
 
 const inlineConfig = {
   "nuxt": {
-    "buildId": "9fc67265-af2a-4625-a4c9-e6649c922b30"
+    "buildId": "4e69c3b7-6688-4bbe-9d7d-0dd62091d70e"
   }
 }
 
--- .nuxt/types/app.config.d.ts
+++ .nuxt/types/app.config.d.ts
@@ -5,7 +5,7 @@ import type { Defu } from 'defu'
 
 declare const inlineConfig = {
   "nuxt": {
-    "buildId": "9fc67265-af2a-4625-a4c9-e6649c922b30"
+    "buildId": "4e69c3b7-6688-4bbe-9d7d-0dd62091d70e"
   }
 }
 type ResolvedAppConfig = Defu<typeof inlineConfig, []>
