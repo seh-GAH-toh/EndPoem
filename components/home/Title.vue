ca9145705278a00aff32411a36c830d0a068fc44
Arthur Segato <github.reentry594@passinbox.com>
2024-05-13 02:56:26 +0100

    Small SEO improvment, Reworked workflows

--- components/home/Title.vue
+++ components/home/Title.vue
@@ -3,10 +3,8 @@
     <h1 class="text-6xl font-bold sm:text-7xl lg:text-8xl 2xl:text-9xl">
       <strong> End Poem </strong>
     </h1>
-    <p
-      class="font-['Nothing_You_Could_Do'] text-3xl sm:text-4xl lg:text-5xl 2xl:text-6xl"
-    >
+    <h2 class="font-['Nothing_You_Could_Do'] text-3xl sm:text-4xl lg:text-5xl 2xl:text-6xl">
       by Julian Gough
-    </p>
+    </h2>
   </header>
 </template>
