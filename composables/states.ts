579234bcd3758d0e2d65440c65134fe347a8fb9a
segato <github.reentry594@passinbox.com>
2025-05-10 18:02:00 +0100

    Clea repo

--- composables/states.ts
+++
@@ -1,3 +0,0 @@
-export const useName = () => useState<string>("name", () => "");
-export const useSubmitted = () => useState<boolean>("submitted", () => false);
-export const useMenu = () => useState<boolean>("menu", () => false);
