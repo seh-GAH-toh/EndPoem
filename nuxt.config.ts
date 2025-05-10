c8f95fde0e4fa5cf97c53552c88c41021822ab20
segato <github.reentry594@passinbox.com>
2025-05-10 16:28:33 +0100

    Add tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,13 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   future: {
     compatibilityVersion: 4,
+  },
+  css: ['~/assets/css/main.css'],
+  vite: {
+    plugins: [tailwindcss()]
   }
 })
