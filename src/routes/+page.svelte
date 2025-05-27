ee1742dea57940d56f06a8e4290d361627432473
segato <github.reentry594@passinbox.com>
2025-05-27 02:03:41 +0100

    Reorganized pages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,10 +5,8 @@
 	import { formState } from '$lib/state.svelte.js';
 </script>
 
-<main class="h-full w-full p-4">
-	{#if !formState.isSubmited}
-		<Home />
-	{:else}
-		<Poem />
-	{/if}
-</main>
+{#if !formState.isSubmited}
+	<Home />
+{:else}
+	<Poem />
+{/if}
