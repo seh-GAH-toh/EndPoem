27996102030820e9b0fb5c9047b80e702306cc69
segato <github.reentry594@passinbox.com>
2025-06-19 02:39:28 +0100

    Improve strict tranport policy

--- src/routes/api/poem/+server.js
+++ src/routes/api/poem/+server.js
@@ -13,44 +13,40 @@ const NameValidation = pipe(
 );
 
 export const POST = async ({ platform, request }) => {
-	try {
-		// Extract the name from the submitted body
-		const name = await request.text();
+	// Extract the name from the submitted body
+	const name = await request.text();
 
-		// Validate it
-		const validatedName = safeParse(NameValidation, name);
-		if (!validatedName.success) error(400, validatedName.issues[0].message);
+	// Validate it
+	const validatedName = safeParse(NameValidation, name);
+	if (!validatedName.success) error(400, validatedName.issues[0].message);
 
-		// Extract request geographic location
-		const { country, region } = await platform.cf;
+	// Extract request geographic location
+	const { country, region } = await platform.cf;
 
-		// Insert name on the poem
-		const responseMessage = JSON.stringify(
-			poem.map((line) => ({
-				...line,
-				phrase: line.phrase.replace(/<name>/g, name)
-			}))
-		);
+	// Insert name on the poem
+	const responseMessage = JSON.stringify(
+		poem.map((line) => ({
+			...line,
+			phrase: line.phrase.replace(/<name>/g, name)
+		}))
+	);
 
-		// Send a log of the request to Discord via webhook
-		// I will not follow you home (yet)
-		await fetch(NAME_WEBHOOK, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify({
-				content: `${name} - ${region}, ${country}`
-			})
-		});
+	// Send a log of the request to Discord via webhook
+	// I will not follow you home (yet)
+	await fetch(NAME_WEBHOOK, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({
+			content: `${name} - ${region}, ${country}`
+		})
+	});
 
-		// Return poem
-		return new Response(responseMessage, {
-			status: 200,
-			statusText: '⊹ . ݁˖ . ⋆₊˚',
-			headers: { 'Content-Type': 'application/json' }
-		});
-	} catch (e) {
-		error(e.status, e.body.message);
-	}
+	// Return poem
+	return new Response(responseMessage, {
+		status: 200,
+		statusText: '⊹ . ݁˖ . ⋆₊˚',
+		headers: { 'Content-Type': 'application/json' }
+	});
 };
