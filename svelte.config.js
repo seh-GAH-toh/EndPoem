5afb402b7eee2c12516a1f7b54add511212408a3
segato <github.reentry594@passinbox.com>
2025-06-19 17:46:09 +0100

    I thin i added the nouce

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,12 @@
 import adapter from '@sveltejs/adapter-cloudflare';
 
-const config = { kit: { adapter: adapter() } };
+const config = {
+    kit: {
+        adapter: adapter(), csp: {
+            mode: "nounce",
+            directives: { "script-src": ["self"] }
+        }
+    }
+};
 
 export default config;
