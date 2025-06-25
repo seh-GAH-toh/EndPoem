495553bb14f334b3ce2cbf5dfdeb325ec4432ca3
segato <github.reentry594@passinbox.com>
2025-06-25 23:01:20 +0100

    Fix speed icon

--- src/lib/states/poem.svelte.js
+++ src/lib/states/poem.svelte.js
@@ -1,29 +1,5 @@
-const Speeds = Object.freeze({
-	Slow: 1500,
-	Medium: 1000,
-	Fast: 600
-});
-
 export const poemState = $state({
 	wasNameSubmited: false,
 	poem: [],
-	speed: Speeds.Medium
+	speed: 1000
 });
-
-export let currentSpeed = Object.keys(Speeds).find((key) => Speeds[key] === poemState.speed);
-
-export const togglePoemSpeed = () => {
-	switch (poemState.speed) {
-		case Speeds.Slow:
-			poemState.speed = Speeds.Medium;
-			break;
-		case Speeds.Medium:
-			poemState.speed = Speeds.Fast;
-			break;
-		case Speeds.Fast:
-			poemState.speed = Speeds.Slow;
-			break;
-		default:
-			break;
-	}
-};
